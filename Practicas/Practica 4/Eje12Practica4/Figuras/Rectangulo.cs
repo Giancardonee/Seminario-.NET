163a54902e3369a4be38b4306c89696d7cd43d64
Gianca <114377978+Giancardonee@users.noreply.github.com>
2024-06-16 20:36:18 -0300

    Add files via upload

---
+++ Practicas/Practica 4/Eje12Practica4/Figuras/Rectangulo.cs
@@ -0,0 +1,15 @@
+﻿namespace Figuras;
+
+public class Rectangulo
+{
+    private double _base; 
+    private double _altura; 
+
+    public Rectangulo(double b, double a){
+        this._base = b; 
+        this._altura = a;
+    }
+
+    public double GetArea => _base * _altura;
+
+}
