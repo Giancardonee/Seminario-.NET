14cf962a83e2939acbd0020c64ca332604b7e64a
Gianca <giancardonee@gmail.com>
2024-07-31 17:33:44 -0300

    Add files via upload

---
+++ Practica 8/Eje1/Consola/Program.cs
@@ -0,0 +1,20 @@
+﻿using clases;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        INombrable[] vector = [
+            new Persona() {Nombre="Zulema"},
+            new Perro() {Nombre="Sultán"},
+            new Persona() {Nombre="Claudia"},
+            new Persona() {Nombre="Carlos"},
+            new Perro() {Nombre="Chopper"},
+            ];
+        Array.Sort(vector);
+        foreach (INombrable n in vector)
+        {
+            Console.WriteLine($"{n.Nombre}: {n}");
+        }
+    }
+}
