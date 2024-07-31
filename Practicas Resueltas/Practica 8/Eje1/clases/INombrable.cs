4a4dfa78b4f6619cf2159f75ea122daaf0dbb4c6
Gianca <giancardonee@gmail.com>
2024-07-31 17:36:20 -0300

    Add files via upload

---
+++ Practicas Resueltas/Practica 8/Eje1/clases/INombrable.cs
@@ -0,0 +1,8 @@
+﻿namespace clases;
+
+public interface INombrable : IComparable<INombrable>
+{
+    string Nombre { get; }
+    string EsUn(); 
+    
+}
---
+++ Practicas Resueltas/Practica 8/Eje1/clases/Perro.cs
@@ -0,0 +1,22 @@
+﻿namespace clases;
+
+public class Perro  : INombrable 
+{
+    public string Nombre { get; set; }="";
+
+    public string EsUn()
+    {
+        return "es un Perro"; 
+    }
+
+    public override string ToString ()
+    {
+        return $"{Nombre} {EsUn()}";
+    }
+
+        public int CompareTo(INombrable? other)
+        {
+            if (other == null) return 1;
+            return string.Compare(this.Nombre, other.Nombre, StringComparison.Ordinal);
+        }
+}
